a26c9aa3d4ed4c89433ab1323b474983aa9536d0
Diana Steshenko <todzuko@gmail.com>
2023-04-12 14:03:26 +0500

    quest api docker

--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/todzuko/go-api
 go 1.20
 
 require (
+	github.com/go-playground/validator/v10 v10.12.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/postgres v1.5.0
+	gorm.io/gorm v1.25.0
+)
+
+require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.12.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
@@ -16,6 +22,4 @@ require (
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	gorm.io/driver/postgres v1.5.0 // indirect
-	gorm.io/gorm v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/todzuko/go-api/controllers"
 	"github.com/todzuko/go-api/models"
 )
 
+const (
+	defaultPort       = "8008"
+	idleTimeout       = 30 * time.Second
+	writeTimeout      = 180 * time.Second
+	readHeaderTimeout = 10 * time.Second
+	readTimeout       = 10 * time.Second
+)
+
 func main() {
+	err := godotenv.Load()
+	check(err)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
 	handler := controllers.New()
 
 	server := &http.Server{
-		Addr:    "0.0.0.0:8008",
+		Addr:    "0.0.0.0:" + port,
 		Handler: handler,
+
+		IdleTimeout:       idleTimeout,
+		WriteTimeout:      writeTimeout,
+		ReadHeaderTimeout: readHeaderTimeout,
+		ReadTimeout:       readTimeout,
 	}
 
 	models.ConnectDatabase()
 
-	server.ListenAndServe()
+	err = server.ListenAndServe()
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
 }
