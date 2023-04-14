1ebdeee1e7e4caa610ac9a555f4ad2288fcdc7be
Diana Steshenko <todzuko@gmail.com>
2023-04-14 08:35:34 +0500

    users CRUD

--- utils/user.go
+++ utils/user.go
@@ -64,6 +64,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(user)
 }
+
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
